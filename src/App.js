550c59ea385da86da84d30b89d7a7d1384cb9b72
AkshaPatel <akshpatel564@gmail.com>
2023-02-01 19:31:18 -0500

    Made changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
+
+function MyButton() {
+  function handleClick() {
+    return (
+      alert('You clicked the button')
+    );
+  }
+  return (
+    <button onClick={handleClick}> 
+      I'm a button 
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World! </h1>
+      <MyButton/>
+
     </div>
   );
 }
