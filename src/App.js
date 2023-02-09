c5f0d3c780bd46c5808a7392901a592ffbb6102a
AkshaPatel <akshpatel564@gmail.com>
2023-02-08 22:16:21 -0500

    Practiced useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
+import { useState } from 'react';
 
-function MyButton() {
-  function handleClick() {
-    return (
-      alert('You clicked the button')
-    );
-  }
-  return (
-    <button onClick={handleClick}> 
-      I'm a button 
-    </button>
-  );
-}
 
 function App() {
+  const [style, setstyle] = useState('');
+
+  function handleClick1() {
+    setstyle('style1');
+  }
+
+  function handleClick2() {
+    setstyle('style2');
+  }
+
   return (
-    <div className="App">
-      <h1> Hello World! </h1>
-      <MyButton/>
+    <div>
+      <h1 className={style}> Change the style by pressing the buttons below: </h1>
+      <button onClick={handleClick1}>Style 1</button>
+      <button onClick={handleClick2}>Style 2</button>
 
     </div>
   );
